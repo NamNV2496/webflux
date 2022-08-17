bc744ad6af93616a6e398b99f367104662d0cbcd
namnv2496 <namnv2496@gmail.com>
2022-08-17 20:04:15 +0700

    add webflux client + server

---
+++ server/src/main/java/com/example/server/model/Pet.java
@@ -0,0 +1,11 @@
+package com.example.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pet   {
+  private Integer id;
+  private String name;
+}
